1b54371df5685adb3a81f6af59b4a8ac7a641d03
Aurino <aurinodoc@gmail.com>
2026-03-02 01:36:19 -0300

    feature: Adição do caso de teste de email inválido com todos os campos preenchidos, criação do README.md e criação dos scripts para execução dos testes em modos headless e open.

---
+++ README.md
@@ -0,0 +1,49 @@
+Páginas analisadas:
+
+1. Site: https://qualidade.apprbs.com.br/site
+2. Certificação: https://qualidade.apprbs.com.br/certificacao
+
+Item 01 - ícone de aba da página ausente
+Tipo: Melhoria
+Classificação: Desejabilidade
+Página(s) relacionada(s): Site e Certificação
+
+Item 02 - ícones da navbar do topo da página pouco destacados
+Tipo: Melhoria
+Classificação: Desejabilidade
+Página(s) relacionada(s): Site
+
+Item 03 - ícone do botão "INSTITUCIONAL" ausente na navbar do topo da página
+Tipo: Melhoria
+Classificação: Desejabilidade
+Página(s) relacionada(s): Site
+
+Item 04 - Botões no carrossel principal não são clicáveis
+Tipo: Melhoria
+Classificação: Usabilidade
+Página(s) relacionada(s): Site
+
+Item 05 - Card quebrado na seção "Conheça nossos diferenciais"
+Tipo: Melhoria
+Classificação: Desejabilidade
+Página(s) relacionada(s): Site
+
+Item 06 - Botão de "Concluir" desarmônico no formulário de newsletter
+Tipo: Melhoria
+Classificação: Usabilidade
+Página(s) relacionada(s): Site
+
+Item 07 - Formulário de newsletter não pode ser submetido
+Tipo: Melhoria
+Classificação: Utilidade
+Página(s) relacionada(s): Site
+
+Item 08 - Card de redes sociais quebrado na parte inferior da página
+Tipo: Melhoria
+Classificação: Desejabilidade
+Página(s) relacionada(s): Site
+
+Item 09 - ícones desarmônicos e fora do padrão na coluna "UNIEXEMPLO Social" do rodapé
+Tipo: Melhoria
+Classificação: Desejabilidade
+Página(s) relacionada(s): Site
--- cypress/e2e/rubeus_practical_test.cy.js
+++ cypress/e2e/rubeus_practical_test.cy.js
@@ -1,47 +1,48 @@
 // Exceção não tratada impedia fluxo padrão de testes do cypress
-Cypress.on('uncaught:exception', (err, runnable) => {
+Cypress.on("uncaught:exception", (err, runnable) => {
   return false;
-})
+});
 
-const newsletter_section = "#inyqq"
-const newsletter_form =  ".rbFormBox .rbFormContainer .rbFormEtapa";
+const newsletter_section = "#inyqq";
+const newsletter_form = ".rbFormBox .rbFormContainer .rbFormEtapa";
 
 const selectors = {
   name_input: `${newsletter_form} .rbFormContent .rbPadding10 .rb-content-form input[name="pessoa.nome"]`,
   email_input: `${newsletter_form} .rbFormContent .rbPadding10 .rb-content-form input[name="pessoa.emailPrincipal"]`,
   phone_input: `${newsletter_form} .rbFormContent .rbPadding10 .rb-content-form .iti input[name="pessoa.telefonePrincipal"]`,
-  submit_button: `${newsletter_form} .rbActionsFormContainer button#rbBtnNext.rbBtnNext`
-}
+  submit_button: `${newsletter_form} .rbActionsFormContainer button#rbBtnNext.rbBtnNext`,
+};
 
 const success_status_codes = [200, 201];
 
-describe('Teste do formulário de newsletter', () => {
-
-  context('Quando todos os dados estão preenchidos', () => {
-
-    context('E todos os dados são válidos', () => {
-
-      it('Deve realizar assinatura da newsletter com sucesso', () => {
+describe("Teste do formulário de newsletter", () => {
+  context("Quando todos os dados estão preenchidos", () => {
+    context("E todos os dados são válidos", () => {
+      it("Deve realizar assinatura da newsletter com sucesso", () => {
         // Arrange
-        cy.visit('/site');
+        cy.visit("/site");
 
         // Act
         cy.get(selectors.name_input).type("Joe Doe");
         cy.get(selectors.email_input).type("valid@email.com");
         cy.get(selectors.phone_input).type("82922223333");
 
-        cy.intercept("PATCH","/api/v2/sendData").as("newsletterSubscription");
-        
+        cy.intercept("PATCH", "/api/v2/sendData").as("newsletterSubscription");
+
         cy.get(selectors.submit_button).click();
 
         cy.wait("@newsletterSubscription").then((interception) => {
-          if (!success_status_codes.includes(interception.response.statusCode)) {
+          if (
+            !success_status_codes.includes(interception.response.statusCode)
+          ) {
             cy.screenshot("fully-filled-newsletter-form-fail").then(() => {
-              throw new Error(`Falha na assinatura da newsletter: status ${interception.response.statusCode} - ${interception.response.statusMessage}`);
+              throw new Error(
+                `Falha na assinatura da newsletter: status ${interception.response.statusCode} - ${interception.response.statusMessage}`,
+              );
             });
           }
         });
-        
+
         // Assert
         cy.get(selectors.name_input).should("have.value", "");
         cy.get(selectors.email_input).should("have.value", "");
@@ -49,82 +50,110 @@ describe('Teste do formulário de newsletter', () => {
         cy.get(selectors.submit_button).should("be.disabled");
 
         cy.screenshot("fully-filled-newsletter-form-success");
-      })
+      });
+    });
 
-    })
+    context("E o campo Email é inválido", () => {
+      it("Deve manter o botão de submissão desabilitado e nenhuma requisição deve ser feita", () => {
+        // Arrange
+        cy.visit("/site");
 
-    context('E o campo Telefone é inválido', () => {
+        // Act
+        cy.get(selectors.name_input).type("Joe Doe");
+        cy.get(selectors.email_input).type("invalid@email.com2");
+        cy.get(selectors.phone_input).type("82922223333");
 
-      context('Porque tem mais digitos do que permitido', () => {
+        cy.intercept("PATCH", "/api/v2/sendData").as("newsletterSubscription");
 
-        it('Deve manter botão de submissão desabilitado e nenhuma requisição deve ser feita', () => {
+        // Assert
+        cy.get(selectors.submit_button).should("be.disabled");
+        cy.get("@newsletterSubscription.all").should("have.length", 0);
+
+        cy.get(newsletter_section).screenshot(
+          "fully-filled-invalid-email-newsletter-form-disabled-button",
+        );
+      });
+    });
+
+    context("E o campo Telefone é inválido", () => {
+      context("Porque tem mais digitos do que permitido", () => {
+        it("Deve manter botão de submissão desabilitado e nenhuma requisição deve ser feita", () => {
           // Arrange
-          cy.visit('/site');
-  
+          cy.visit("/site");
+
           // Act
           cy.get(selectors.name_input).type("Joe Doe");
           cy.get(selectors.email_input).type("invalid@email.com2");
           cy.get(selectors.phone_input).type("829111133334");
-  
-          cy.intercept("PATCH","/api/v2/sendData").as("newsletterSubscription");
-          
+
+          cy.intercept("PATCH", "/api/v2/sendData").as(
+            "newsletterSubscription",
+          );
+
           // Assert
           cy.get(selectors.submit_button).should("be.disabled");
           cy.get("@newsletterSubscription.all").should("have.length", 0);
-  
-          cy.get(newsletter_section).screenshot("required-invalid-email-newsletter-form-disabled-button");
-        })
 
+          cy.get(newsletter_section).screenshot(
+            "required-invalid-email-newsletter-form-disabled-button",
+          );
+        });
       });
 
-      context('Porque tem menos digitos do que permitido', () => {
-        
-        it('Deve manter botão de submissão desabilitado e nenhuma requisição deve ser feita', () => {
+      context("Porque tem menos digitos do que permitido", () => {
+        it("Deve manter botão de submissão desabilitado e nenhuma requisição deve ser feita", () => {
           // Arrange
-          cy.visit('/site');
-  
+          cy.visit("/site");
+
           // Act
           cy.get(selectors.name_input).type("Joe Doe");
           cy.get(selectors.email_input).type("invalid@email.com2");
           cy.get(selectors.phone_input).type("8291111332");
-  
-          cy.intercept("PATCH","/api/v2/sendData").as("newsletterSubscription");
-          
+
+          cy.intercept("PATCH", "/api/v2/sendData").as(
+            "newsletterSubscription",
+          );
+
           // Assert
           cy.get(selectors.submit_button).should("be.disabled");
           cy.get("@newsletterSubscription.all").should("have.length", 0);
-  
-          cy.get(newsletter_section).screenshot("required-invalid-email-newsletter-form-disabled-button");
-        })
 
-      })
-
+          cy.get(newsletter_section).screenshot(
+            "required-invalid-email-newsletter-form-disabled-button",
+          );
+        });
+      });
     });
   });
 
-  context('Quando somente os dados obrigatórios estão preenchidos', () => {
-
-    context('E todos os dados são válidos', () => {
-      it('Deve realizar assinatura da newsletter com sucesso', () => {
+  context("Quando somente os dados obrigatórios estão preenchidos", () => {
+    context("E todos os dados são válidos", () => {
+      it("Deve realizar assinatura da newsletter com sucesso", () => {
         // Arrange
-        cy.visit('/site');
+        cy.visit("/site");
 
         // Act
         cy.get(selectors.name_input).type("Joe Doe");
         cy.get(selectors.email_input).type("valid@email.com");
 
-        cy.intercept("PATCH","/api/v2/sendData").as("newsletterSubscription");
-        
+        cy.intercept("PATCH", "/api/v2/sendData").as("newsletterSubscription");
+
         cy.get(selectors.submit_button).click();
 
         cy.wait("@newsletterSubscription").then((interception) => {
-          if (!success_status_codes.includes(interception.response.statusCode)) {
-            cy.screenshot("required-fields-filled-newsletter-form-fail").then(() => {
-              throw new Error(`Falha na assinatura da newsletter: status ${interception.response.statusCode} - ${interception.response.statusMessage}`);
-            });
+          if (
+            !success_status_codes.includes(interception.response.statusCode)
+          ) {
+            cy.screenshot("required-fields-filled-newsletter-form-fail").then(
+              () => {
+                throw new Error(
+                  `Falha na assinatura da newsletter: status ${interception.response.statusCode} - ${interception.response.statusMessage}`,
+                );
+              },
+            );
           }
         });
-        
+
         // Assert
         cy.get(selectors.name_input).should("have.value", "");
         cy.get(selectors.email_input).should("have.value", "");
@@ -132,34 +161,35 @@ describe('Teste do formulário de newsletter', () => {
         cy.get(selectors.submit_button).should("be.disabled");
 
         cy.screenshot("required-fields-filled-newsletter-form-success");
-      })
+      });
     });
 
-    context('E o campo Email é inválido', () => {
-      it('Deve manter o botão de submissão desabilitado e nenhuma requisição deve ser feita', () => {
+    context("E o campo Email é inválido", () => {
+      it("Deve manter o botão de submissão desabilitado e nenhuma requisição deve ser feita", () => {
         // Arrange
-        cy.visit('/site');
+        cy.visit("/site");
 
         // Act
         cy.get(selectors.name_input).type("Joe Doe");
         cy.get(selectors.email_input).type("invalid@email.com2");
 
-        cy.intercept("PATCH","/api/v2/sendData").as("newsletterSubscription");
-        
+        cy.intercept("PATCH", "/api/v2/sendData").as("newsletterSubscription");
+
         // Assert
         cy.get(selectors.submit_button).should("be.disabled");
         cy.get("@newsletterSubscription.all").should("have.length", 0);
 
-        cy.get(newsletter_section).screenshot("required-invalid-email-newsletter-form-disabled-button");
-      })
+        cy.get(newsletter_section).screenshot(
+          "required-invalid-email-newsletter-form-disabled-button",
+        );
+      });
     });
+  });
 
-  })
-
-  context('Quando todos os campos do formulário estão vazios', () => {
-    it('Deve ter o botão de submissão desabilitado e nenhuma requisição deve ser feita', () => {
+  context("Quando todos os campos do formulário estão vazios", () => {
+    it("Deve ter o botão de submissão desabilitado e nenhuma requisição deve ser feita", () => {
       // Arrange
-      cy.visit('/site');
+      cy.visit("/site");
 
       // Act
       cy.intercept("PATCH", "/api/v2/sendData").as("newsletterSubscription");
@@ -168,14 +198,16 @@ describe('Teste do formulário de newsletter', () => {
       cy.get(selectors.submit_button).should("be.disabled");
       cy.get("@newsletterSubscription.all").should("have.length", 0);
 
-      cy.get(newsletter_section).screenshot("empty-newsletter-form-disabled-button");
-    })
+      cy.get(newsletter_section).screenshot(
+        "empty-newsletter-form-disabled-button",
+      );
+    });
   });
 
-  context('Quando somente o campo de Nome está preenchido', () => {
-    it('Deve ter o botão de submissão desabilitado e nenhuma requisição deve ser feita', () => {
+  context("Quando somente o campo de Nome está preenchido", () => {
+    it("Deve ter o botão de submissão desabilitado e nenhuma requisição deve ser feita", () => {
       // Arrange
-      cy.visit('/site');
+      cy.visit("/site");
 
       // Act
       cy.get(selectors.name_input).type("Joe Doe");
@@ -186,14 +218,16 @@ describe('Teste do formulário de newsletter', () => {
       cy.get(selectors.submit_button).should("be.disabled");
       cy.get("@newsletterSubscription.all").should("have.length", 0);
 
-      cy.get(newsletter_section).screenshot("name-filled-newsletter-form-disabled-button");
-    })
+      cy.get(newsletter_section).screenshot(
+        "name-filled-newsletter-form-disabled-button",
+      );
+    });
   });
-  
-  context('Quando somente o campo de Email está preenchido e é válido', () => {
-    it('Deve ter o botão de submissão desabilitado e nenhuma requisição deve ser feita', () => {
+
+  context("Quando somente o campo de Email está preenchido e é válido", () => {
+    it("Deve ter o botão de submissão desabilitado e nenhuma requisição deve ser feita", () => {
       // Arrange
-      cy.visit('/site');
+      cy.visit("/site");
 
       // Act
       cy.get(selectors.email_input).type("valid@email.com");
@@ -204,8 +238,9 @@ describe('Teste do formulário de newsletter', () => {
       cy.get(selectors.submit_button).should("be.disabled");
       cy.get("@newsletterSubscription.all").should("have.length", 0);
 
-      cy.get(newsletter_section).screenshot("valid-email-filled-newsletter-form-disabled-button");
-    })
+      cy.get(newsletter_section).screenshot(
+        "valid-email-filled-newsletter-form-disabled-button",
+      );
+    });
   });
-
-})
+});
